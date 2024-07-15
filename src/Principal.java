3afa5fb6ca2dd7bb729e0976d498d0afcf3934a1
Felipe Guimarães <141097410+felipeguimares@users.noreply.github.com>
2024-07-15 11:28:19 -0300

    01

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,8 @@ import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
@@ -50,5 +52,19 @@ public class Principal {
         episodio.setSerie(lost);
         episodio.setTotalVisualizacoes(300);
         filtro.filtra(episodio);
+        
+        Filme filmeDoFelipe = new Filme();
+        filmeDoFelipe.setNome("Dogville");
+        filmeDoFelipe.setDuracaoEmMinutos(200);
+        filmeDoFelipe.setAnoDeLancamento(2003);
+        filmeDoFelipe.avalia(10);
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(filmeDoFelipe);
+        listaDeFilmes.add(meuFilme);
+        listaDeFilmes.add(outroFilme);
+        System.out.println("Tamanho da lista " + listaDeFilmes.size());
+        System.out.println("Meu primeiro filme " +listaDeFilmes.get(0).getNome());
+        System.out.println(listaDeFilmes);
     }
 }
